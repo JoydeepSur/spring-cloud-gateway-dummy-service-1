1674fbe2303cd67dd49bcecd04658d5d3c3120d6
Joydeep Sur <joydeepsur6@gmail.com>
2020-04-28 12:47:46 +0530

    initial commit

---
+++ src/main/java/com/example/service/SpringCloudGatewayDummyService1Application.java
@@ -0,0 +1,13 @@
+package com.example.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayDummyService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatewayDummyService1Application.class, args);
+	}
+
+}
